e18c6c025e1e7b02cc242c21fec5d82b1fe72768
Edward Jones <ed.f.jones@gmail.com>
2021-08-16 16:54:35 -0400

    gallery omplete

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ this.prevBtn = getElement('.prev-btn');
 this.closeModal = this.closeModal.bind(this);
 this.nextImage = this.nextImage.bind(this);
 this.prevImage = this.prevImage.bind(this);
+this.chooseImage = this.chooseImage.bind(this);
 // bind function
 //this.openModal = this.openModal.bind(this);
 
@@ -49,6 +50,7 @@ Gallery.prototype.openModal = function(selectedImage,list){
   this.closeBtn.addEventListener('click', this.closeModal);
   this.nextBtn.addEventListener('click', this.nextImage);
   this.prevBtn.addEventListener('click', this.prevImage);
+  this.modalImages.addEventListener('click', this. chooseImage);
 };
 
 Gallery.prototype.setMainImage = function(selectedImage){
@@ -61,15 +63,33 @@ Gallery.prototype.closeModal = function() {
   this.closeBtn.removeEventListener('click', this.closeModal);
   this.nextBtn.removeEventListener('click', this.nextImage);
   this.prevBtn.removeEventListener('click', this.prevImage);
+  this.modalImages.removeEventListener('click', this.chooseImage);
 }
 
 Gallery.prototype.nextImage = function(){
-
+  const selected = this.modalImages.querySelector('.selected');
+  const next = selected.nextElementSibling || this.modalImages.firstElementChild;
+  selected.classList.remove('selected');
+  next.classList.add('selected');
+  this.setMainImage(next);
 };
 
 Gallery.prototype. prevImage = function(){
-
+  const selected = this.modalImages.querySelector('.selected');
+  const prev = selected.previousElementSibling || this.modalImages.lastElementChild;
+  selected.classList.remove('selected');
+  prev.classList.add('selected');
+  this.setMainImage(prev);
 };
+
+Gallery.prototype.chooseImage = function (e) {
+  if(e.target.classList.contains('modal-img')) {
+    const selected = this.modalImages.querySelector('.selected');
+    selected.classList.remove('selected');
+    this.setMainImage(e.target);
+    e.target.classList.add('selected');
+  };
+}
 
 const nature = new Gallery(getElement('.nature'));
 const city = new Gallery(getElement('.city'));
